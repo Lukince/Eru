317d9ea37f3de04b99f98d80b04ba2ca4be431ff
Lukince <whalstjd57@gmail.com>
2019-08-13 11:01:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ dscl.on("message", (message)=> {
     if (cmd == `${prefix}안녕`) {
         //console.log(message.mentions.user.id)
         return message.channel.send(HelloArr[RandInt(1)]);
+
     } else if (cmd == `${prefix}체크`) {
         if (add == "출석") {
             return message.channel.send(`${message.author} 출석 체크 되었습니다!`);
@@ -69,6 +70,7 @@ dscl.on("message", (message)=> {
 
             return message.channel.send(`${message.author} 알수 없는 인수입니다. "^도움말 체크" 를 사용해 주세요.`);
         }
+
     } else if (cmd == `${prefix}정보`) {
         let User = message.mentions.users.first() || message.author
         if (User.id == '538681468679880736') {
@@ -97,6 +99,7 @@ dscl.on("message", (message)=> {
             .addField('서버 가입일', `${membertime.format('YYYY MMM Do, h:mm:ss a')}`, false)
             .addField('계정 생성일', `${usertime.format('YYYY MMM Do, h:mm:ss a')}`, true)
         message.channel.send(UserInfoEmbed)
+
     } else if (cmd == `${prefix}도움말`) {
         if(add == '체크') {
             let checkHelp = new discord.RichEmbed()
@@ -109,6 +112,7 @@ dscl.on("message", (message)=> {
                 .addField('^체크 서버', '서버에 대한 정보를 확인합니다.')
 
             message.author.send(checkHelp)
+
         } else if (add == '투표') {
             let voteHelp = new discord.RichEmbed()
                 .setColor(ColorArr[RandInt(11)])
@@ -119,6 +123,7 @@ dscl.on("message", (message)=> {
                 .addField('예시 사용법', '^투표 .맑은 날씨다')
 
             message.author.send(voteHelp)
+
         } else if (add == '선택') {
             let chooseHelp = new discord.RichEmbed()
                 .setColor(ColorArr[RandInt(11)])
@@ -131,6 +136,7 @@ dscl.on("message", (message)=> {
             //,(콤마)를 기준으로 나누는거 해결하기
 
             message.author.send(chooseHelp)
+
         } else if (add == null) {
             let helpEmbed = new discord.RichEmbed()
                 .setColor(ColorArr[RandInt(11)])
@@ -149,6 +155,7 @@ dscl.on("message", (message)=> {
                 .addField('코드 관련', '"^js" discord.js 문서 Url "^소스" Eru봇 소스입니다.')
 
             message.author.send(helpEmbed)
+
         } else if (add == '연산') {
             let CalcEmbed = new discord.RichEmbed()
                 .setColor(ColorArr[RandInt(11)])
@@ -161,6 +168,7 @@ dscl.on("message", (message)=> {
                 .addField('주의!', '다른 유저가 문제를 푸는 중일경우 문제를 받은 사람을 제외한 모든 사람은 ^연산 과 ^답 명령어를 사용할 수 없습니다. 반올림 시스템이 구현이 안되어 "÷"가 나왔을때 정확한 값(소숫점 16자리까지)을 입력해야 합니다.')
 
             message.author.send(CalcEmbed)
+
         } else {
             return message.channel.send(`${message.author} 알수 없는 인수입니다. "^help" 를 사용해 주세요`)
         }
@@ -168,14 +176,17 @@ dscl.on("message", (message)=> {
 
     } else if (cmd == `${prefix}말하기`) {
         message.channel.send(add);
+
     } else if (cmd == `${prefix}DM`) {
         message.author.send(`${message.author.id}`);
+
     } else if (cmd == `${prefix}투표`) {
         message.channel.send(select[1]).then(sentMessage => {
             sentMessage.react('✅')
                 .then(() => sentMessage.react('❌'))
                 .catch(() => console.log('투표 구문 에러 code:1 js 126~132'))
         }) .catch(() => message.channel.send(`${message.author} 인수를 찾을 수 없습니다. "^도움말 투표" 를 사용해 주세요`))
+
     } else if (cmd == `${prefix}선택`) {
         console.log(select[0])
         console.log(select[1])
@@ -222,14 +233,17 @@ dscl.on("message", (message)=> {
 
         } else if (select[6] != null) message.channel.send(`${message.author} 인수 값의 최대는 4개 입니다.`);
         else message.channel.send(`${message.author} "^도움말 선택" 을 참고해 주세요`);
+
     } else if (cmd == `${prefix}kick`) {
         if (roll.hasPermission('BAN_MEMBERS')) {
             console.log('이 유저는 ban이 불가능 합니다.');
             return;
         }
         console.log('이 유저는 ban이 가능합니다.');
+
     } else if (cmd == `${prefix}주사위`) {
         return message.channel.send(RollArr[RandInt(5)]);
+
     } else if (cmd == `${prefix}연산`) {
         if (WaitAnswer == 0) {
             NowUser = message.author.username
@@ -277,12 +291,15 @@ dscl.on("message", (message)=> {
                 WaitAnswer = 0
             }
         }
+
     } else if (cmd == `${prefix}답` && WaitAnswer == 1) {
         console.log(message.author.id)
         console.log(answerid)
         message.channel.send(`${message.author} 문제 출제자만 정답을 입력할 수 있습니다.`)
+
     } else if (cmd == `${prefix}답` && WaitAnswer == 0) {
         message.channel.send(`문제를 푸는중이 아닙니다!`)
+
     } else if (cmd == `${prefix}nsfw` || cmd == `${prefix}NSFW`) {
         if (message.channel.id == '457331588858904586') {
             message.member.addRole('476377978771603467')
@@ -290,6 +307,7 @@ dscl.on("message", (message)=> {
         } else {
             message.channel.send('사용 가능한 채널 또는 서버가 아닙니다.')
         }
+
     } else if (cmd == `${prefix}가위바위보`) {
         let Paper = 0
         let BotPaper = PaperArr[RandInt(2)]
@@ -331,6 +349,7 @@ dscl.on("message", (message)=> {
                 return message.channel.send(`${message.author} 이런! 알수 없는 오류가 발생했내요`)
             }
         }
+
     } else if (cmd == `${prefix}말했기`) {
         if (select[1] == null) {
             return message.channel.send(`${message.author} 인수가 없습니다. .을 이용하여 인수를 설정해 주세요`)
@@ -338,6 +357,7 @@ dscl.on("message", (message)=> {
         message.delete(0)
         .then(() => message.channel.send(select[1]))
         .catch(() => hook.send(`${message.guild.name} 서버에서 채팅 지우기 권한이 존재하지 않습니다.`))
+
     } else if (cmd == `${prefix}반응`) {
         if (add == null) {
             return message.channel.send(`원하는 이모지를 써주세요 EX) :thinking:`)
@@ -347,23 +367,28 @@ dscl.on("message", (message)=> {
             
     } else if (message.isMentioned('538681468679880736')) {
         message.channel.send(`나불렀엉?`)
+
     } else if (cmd == `${prefix}타이핑`) {
         if (message.author.id == '378535260754935819') return message.channel.startTyping()
         else if (message.author.id != '378535260754935819') {
             message.channel.send(`${message.author} 봇 관리자 전용 메시지 입니다. 또는 실험중인 명령어 입니다.`)
         }
+
     } else if (cmd == `${prefix}중지`) {
         if (message.author.id == '378535260754935819') message.channel.stopTyping()
         else if (message.author.id != '378535260754935819') {
             message.channel.send(`${message.author} 봇 관리자 전용 메시지 입니다. 또는 실험중인 명령어 입니다.`)
         }
+
     } else if (cmd == `${prefix}js`) {
         message.channel.send('https://discord.js.org/')
+
     } else if (cmd == `${prefix}토마스`) {
         let TomasEmbed = new discord.RichEmbed()
             .setColor(ColorArr[RandInt(11)])
             .setImage('https://cdn.discordapp.com/attachments/447787026754830337/609992044122734612/20190810_192851.jpg')
         message.channel.send(TomasEmbed)
+
     } else if (cmd == `${prefix}추가요청`) {
         message.react(':thumbsup:')
         message.channel.send(`${message.author} 추가요청이 확인 되었습니다!`)
@@ -371,18 +396,34 @@ dscl.on("message", (message)=> {
             .setTitle('추가요청')
             .addField(`${message.author.username}님의 추가요청`, (`내용 : ${message.content}`))
         Addhook.send(AddEmbed)
+
     } else if (cmd == `${prefix}Embed`) {
         let RandomEmbed = new discord.RichEmbed()
             .setTitle('무작위 Embed 소환!')
             .setColor(ColorArr[RandInt(11)])
             .addField('내용', '없찌롱~', true)
         message.channel.send(RandomEmbed)
+
     } else if (cmd == `${prefix}샌즈`) {
         message.channel.send('https://giphy.com/gifs/UCkZPALajEs8M')
         message.channel.send('와! 샌즈!')
+
+    } else if (cmd == `${prefix}AddRole`) {
+        if (message.author.id == '378535260754935819') {
+            if (add == null) (`${message.author} 인수가 존재하지 않습니다.`)
+            guild.createRole({
+                name: select[1],
+                color: select[2],
+              })
+        }
+        else if (message.author.id != '378535260754935819') {
+            message.channel.send(`${message.author} 봇 관리자 전용 메시지 입니다. 또는 실험중인 명령어 입니다.`)
+        }
+
     } else if (cmd == `${prefix}소스`) {
         message.author.send('https://github.com/Lukince/Eru/blob/master/index.js')
         message.author.send('현재 EruBot은 Node.js에 대해 오픈소스를 제공하고 있습니다. 복붙하시면 맞아 죽습니다^^')
+
     } else if (check[0] == prefix) {
         if (check[1] != " ") {
             message.channel.send(`> ${message.author} 아직은 그런거 모르는데..`)
