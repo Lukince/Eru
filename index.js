5881123f84b78519498491eab028de4d9bcf9628
Lukince <53417057+Lukince@users.noreply.github.com>
2019-08-15 10:11:18 +0900

    Version 3.1.1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function print() {
 
 let PaperArr = ["가위", "바위", "보"]
 let HelloArr = ["안녕 난 이루야 :kissing_heart:", "안녀엉! :laughing:"]
-let version = 'Version 3.1.0 Patch Data : 2019/08/15'
+let version = 'Version 3.1.1 Patch Data : 2019/08/15'
 let CalcArr = ["+", "-", "×", "÷"]
 let ColorArr = ['#FF0000', '#FFA500', '#FFFF00', '#00FF00', '#008000', '#87ceeb', '#0000FF', '#800080', '#FFC0CB', '#ffffff', '#000000', '#808080'] 
                 //빨강, 주황, 노랑, 연두(라임), 초록, 하늘(skyblue), 파랑, 보라, 분홍, 하얀, 검정, 회색
@@ -51,6 +51,7 @@ dscl.on("message", (message)=> {
     let cmd = msg[0];
     let add = msg[1];
     let i = 0;
+    let uptime = client.uptime
 
     const filter = m => m.content.split(" ") == `${prefix}연산`
     if (WaitAnswer == 1) {
@@ -458,8 +459,9 @@ dscl.on("message", (message)=> {
             .addField('주사위 개편', '이제부터 ^주사위 를 사용하면 숫자가 아닌 이모지로 대체되서 나옵니다!')
             .addField('연산 개편', '이제부터 ^연산 사용에 ÷(나누기)가 나올 경우 소숫점 둘째 자리에서 반올림 하여 값을 나타냅니다!')
             .addField('이스터에그', '이스터에그가 추가되었습니다. 한번 찾아보세요!')
+            .addField('패치 내역 3.1.1', '패치내역에 Uptime(구동시간)이 표시됨')
             .setColor(ColorArr[RandInt(11)])
-            .setFooter(version)
+            .setFooter(`패치 이후 uptime : ${uptime}`)
         message.channel.send(PatchEmbed)
     } else if (check[0] == prefix) {
         if (check[1] != " ") {
