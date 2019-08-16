3f11b4c364a8c9af686649fb913aea7a81374967
Lukince <53417057+Lukince@users.noreply.github.com>
2019-08-17 00:28:25 +0900

    3.2.0e

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let HelloArr = ["안녕 난 이루야 :kissing_heart:", "안녀엉! :laughing:"]
 let version = 'Version 3.2.0 Patch Data : 2019/08/17'
                     //       대버전         .        중버전      .     소버전
                     //3개 이상의 명령어 생성 . 1~2개의 명령어 생성 . 간단한 오류 수정
-let SetupVersion = 'Version 3.2.0d Patch Data : 2019/08/17'
+let SetupVersion = 'Version 3.2.0e Patch Data : 2019/08/17'
 let CalcArr = ["+", "-", "×", "÷"]
 let ColorArr = ['#FF0000', '#FFA500', '#FFFF00', '#00FF00', '#008000', '#87ceeb', '#0000FF', '#800080', '#FFC0CB', '#ffffff', '#000000', '#808080'] 
                 //빨강, 주황, 노랑, 연두(라임), 초록, 하늘(skyblue), 파랑, 보라, 분홍, 하얀, 검정, 회색
@@ -503,8 +503,7 @@ client.on("message", (message)=> {
         let IsPrime = true
         let TempPrime = `약수는 `
         message.channel.send(add)
-        if (isNaN(add) == false) return message.channel.send(`인수가 숫자가 아닙니다!`)
-        if (Number.isInteger(add) == false) return message.channel.send(`숫자가 정수가 아닙니다!`)
+        if (add % 2 == 1 || add % 2 == 2) return message.channel.send(`숫자가 정수가 아닙니다!`)
         if (add < 1) return message.channel.send(`인수가 1보다 작습니다!`)
         while (add == prime) {
             if (Number.isInteger(add / prime)) {
