88dedf271188a0f0d297bf271c9210cf067ec0db
Lukince <53417057+Lukince@users.noreply.github.com>
2019-10-12 14:22:22 +0900

    3.2.1
    bug fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const Addhook = new discord.WebhookClient(process.env.AddId, process.env.AddToke
 
 client.on("ready", () => {
     console.log(`${client.user.username}is Online!`);
-    client.channels.get('632428472920309760').setName(`Ping : ${client.ping}`)
+    client.channels.get('632428472920309760').setName(`Ping : ${Math.round(client.ping)}`)
     client.user.setActivity(`${client.guilds.size}개의 서버에서 ${client.users.size}명이 사용중!`, { type: "STREAMING" , url : 'https://twitch.tv/lukince'});
 });
 
