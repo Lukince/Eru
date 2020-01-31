4b0a573098d91c9a6672a6c220a9029550f34947
Lukince <53417057+Lukince@users.noreply.github.com>
2020-01-31 12:49:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function RandInt(max) {
 }
 
 function emoji(add) {
-    emojid = client.emojis.get(add).toString()
+    emojid = client.emojis.get(add)
         .catch(() => message.channel.send('emoji의 번호가 올바르지 않거나 봇이 해당 서버에 있지 않습니다.'));
     return emojid;
 }
